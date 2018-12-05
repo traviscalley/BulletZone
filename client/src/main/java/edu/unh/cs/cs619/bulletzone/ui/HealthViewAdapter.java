eafd82756c9d78d22be5fdc74ae9400f9bc49ee9
Alexander V Saveliev <alexsave@github.com>
2018-12-04 21:04:35 -0500

    almost got health showing

---
+++ client/src/main/java/edu/unh/cs/cs619/bulletzone/ui/HealthViewAdapter.java
@@ -0,0 +1,30 @@
+package edu.unh.cs.cs619.bulletzone.ui;
+
+import android.content.Context;
+import android.widget.LinearLayout;
+import android.widget.TextView;
+
+import com.squareup.otto.Subscribe;
+
+public class HealthViewAdapter {
+    Context context;
+    LinearLayout linearLayout;
+
+    public HealthViewAdapter(Context c, LinearLayout ll){
+        this.context = c;
+        this.linearLayout = ll;
+    }
+
+    @Subscribe
+    public void addHealthInfo(HealthInfoEvent event) {
+        TextView valueTV = new TextView(context);
+        valueTV.setText(event.healths);
+        //valueTV.setId(5);
+        valueTV.setLayoutParams(new LinearLayout.LayoutParams(
+                LinearLayout.LayoutParams.MATCH_PARENT,
+                LinearLayout.LayoutParams.WRAP_CONTENT));
+
+        linearLayout.addView(valueTV);
+    }
+
+}
